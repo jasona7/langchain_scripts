a28e362cd167ef3a719577e44e7a2c3d3aa85806
Jason <jasonalloway1@gmail.com>
2023-04-28 10:24:07 -0400

    Update bash_oneliners_langchain.py port

--- bash_oneliners_langchain.py
+++ bash_oneliners_langchain.py
@@ -66,4 +66,4 @@ def serve_image(filename):
     return send_from_directory('static', filename, mimetype='image/png')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=9922, debug=True)
+    app.run(host='0.0.0.0', port=80, debug=True)
